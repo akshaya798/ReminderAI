dfe2b69bcf7b7646f6648a44d28184c508be30c9
akshaya k <2023it0842@svce.ac.in>
2026-01-28 10:26:30 +0000

    Build for GitHub Pages

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>AMC Reminder & Escalation</h1>
+      <p>Welcome to ReminderAI</p>
+    </div>
+  )
+}
+
+export default App
